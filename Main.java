0da96556487a3b24d4b3d72cdc689615e5747c46
Diego Alejandro Bedoya <157883794+dabedoyav@users.noreply.github.com>
2024-05-10 19:45:17 -0500

    commit uno

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+    metodoimprimir();
+    }
+
+    static void metodoimprimir(){
+        System.out.println("Assignment 3 by Diego Alejandro Bedoya");
+    }
+}
